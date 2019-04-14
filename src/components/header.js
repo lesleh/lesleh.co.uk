3268d453fb924191592f87161aba0c3cfdaf31ed
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-15 00:44:39 +0100

    Fix mobile menu opening

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { Component } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Header = () => (
-  <header className="site-header">
-    <h1><span>Lesleh</span>.co.uk</h1>
-    <button className="site-nav__hamburger" aria-label="Main menu"><i className="fas fa-bars"></i></button>
-  </header>
-)
+const Header = class extends Component {
+  handleHamburgerClicked = () => {
+    let menu = document.querySelector('.site-nav')
+    if(menu) {
+      menu.classList.toggle('is-open')
+    }
+  }
+
+  render () {
+    return(
+      <header className="site-header">
+        <h1><span>Lesleh</span>.co.uk</h1>
+        <button
+          className="site-nav__hamburger"
+          aria-label="Main menu"
+          onClick={this.handleHamburgerClicked}
+        >
+          <FontAwesomeIcon icon={faBars} fixedWidth />
+        </button>
+      </header>
+    )
+  }
+}
 
 export default Header
