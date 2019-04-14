e3286a50ca412376d78168d5d83d05fe9b19109b
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-14 23:55:49 +0100

    Track active link in nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,22 +4,22 @@ import { Link } from "gatsby"
 const Nav = () => (
   <ul className="site-nav">
     <li>
-      <Link to="/">
+      <Link to="/" activeClassName='active'>
         Home <i className="fas fa-fw fa-home"></i>
       </Link>
     </li>
     <li>
-      <Link to="/about/">
+      <Link to="/about/" activeClassName='active'>
         About <i className="fas fa-fw fa-user-circle"></i>
       </Link>
     </li>
     <li>
-      <Link to="/photos/">
+      <Link to="/photos/" activeClassName='active'>
         Photos <i className="fas fa-fw fa-images"></i>
       </Link>
     </li>
     <li>
-      <Link to="/contact/">
+      <Link to="/contact/" activeClassName='active'>
         Contact <i className="fas fa-fw fa-envelope"></i>
       </Link>
     </li>
