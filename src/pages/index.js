bb566e0e65e9b2fbee4986d29cb717da63f5d973
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-15 17:02:25 +0100

    Show blog post listing on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,39 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-const IndexPage = () => (
-  <Layout title="Home">
+const IndexPage = ({ data }) => (
+  <Layout title="Posts">
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3>
+          {node.frontmatter.title}{" "}
+          <span>
+            — {node.frontmatter.date}
+          </span>
+        </h3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
