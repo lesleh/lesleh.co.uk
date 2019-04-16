246dafdf920dd2c9f52ec6ba118bce765f695df0
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-16 11:45:14 +0100

    Refactor photo gallery

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const Image = () => (
-  <a className="js-gallery-link gallery__link gallery__link--small" href="https://lesleh.co.uk/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbTRDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2297539394b065fd2909059e2634aa53c7489dba/51703156_422624894946935_2735823648280980910_n.jpg%3F_nc_ht=scontent.cdninstagram.com">
-    <img className="gallery__image" style={{ minWidth: '100px', minHeight: '100px' }} alt="" src="https://lesleh.co.uk/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbTRDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2297539394b065fd2909059e2634aa53c7489dba/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9MY21WemFYcGxTU0lNTlRBd2VEVXdNQVk2QmtWVSIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--fb979cd7246ee8b644d965793b279f878ea159a2/51703156_422624894946935_2735823648280980910_n.jpg%3F_nc_ht=scontent.cdninstagram.com" />
+const Image = ({ url }) => (
+  <a className="js-gallery-link gallery__link gallery__link--small" href={url}>
+    <img className="gallery__image" style={{ minWidth: '100px', minHeight: '100px' }} alt="" src={url} />
   </a>
 );
 
-const Photos = () => (
+const Photos = ({ data }) => (
   <Layout title="Photos">
     <div className="gallery">
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
-      <Image />
+      { data.allFile.edges.map(edge => (
+        <Image url={edge.node.childImageSharp.original.src} />
+      )) }
     </div>
   </Layout>
 );
 
 export default Photos;
+
+export const query = graphql`
+  {
+    allFile(sort: {order: DESC, fields: [name]}, filter: {relativePath: {regex: "/photos/"}}) {
+      edges {
+        node {
+          childImageSharp {
+            id
+            original {
+              width
+              height
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`;
