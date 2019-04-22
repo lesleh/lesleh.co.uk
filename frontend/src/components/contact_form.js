03d209f0c7eca6a0188456da5e5658eeb11349d6
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-22 17:34:14 +0100

    Add correct contact form submit URL

--- frontend/src/components/contact_form.js
+++ frontend/src/components/contact_form.js
@@ -1,48 +1,48 @@
 import React from 'react';
 import Reaptcha from 'reaptcha';
 
-import ContactMailer from '../services/contact_mailer';
+// import ContactMailer from '../services/contact_mailer';
 
 export default class ContactForm extends React.Component {
-  state = {
-    recaptchaVerified: false,
-  }
+  // state = {
+  //   recaptchaVerified: false,
+  // }
 
   verifyCallback = () => {
-    this.setState({
-      recaptchaVerified: true,
-    });
+    // this.setState({
+    //   recaptchaVerified: true,
+    // });
   }
 
-  handleSubmit = async (e) => {
-    const { recaptchaVerified } = this.state;
-    e.persist();
-    e.preventDefault();
-    const mailer = new ContactMailer();
-    try {
-      await mailer.send(new FormData(e.target));
-    } catch (ex) {
-      // eslint-disable-next-line no-console
-      console.error(ex);
-    }
-    e.preventDefault();
-    if (!recaptchaVerified) {
-      // eslint-disable-next-line no-alert
-      window.alert('Please complete captcha verification first');
-      e.preventDefault();
-    }
+  handleSubmit = async () => {
+    // const { recaptchaVerified } = this.state;
+    // e.persist();
+    // e.preventDefault();
+    // const mailer = new ContactMailer();
+    // try {
+    //   await mailer.send(new FormData(e.target));
+    // } catch (ex) {
+    //   // eslint-disable-next-line no-console
+    //   console.error(ex);
+    // }
+    // e.preventDefault();
+    // if (!recaptchaVerified) {
+    //   // eslint-disable-next-line no-alert
+    //   window.alert('Please complete captcha verification first');
+    //   e.preventDefault();
+    // }
   }
 
   render() {
     return (
-      <form noValidate onSubmit={this.handleSubmit} className="simple_form form" id="new_contact_message" action="/contact/" acceptCharset="UTF-8" method="post">
+      <form noValidate onSubmit={this.handleSubmit} className="simple_form form" id="new_contact_message" action="https://vv7mpokoyg.execute-api.eu-west-1.amazonaws.com/dev/contact" acceptCharset="UTF-8" method="post">
         <div className="input string required contact_message_name">
           <label className="string required" htmlFor="contact_message_name">
             <abbr title="required">*</abbr>
             {' '}
             Name
           </label>
-          <input required className="string required" type="text" name="contact_message[name]" id="contact_message_name" />
+          <input required className="string required" type="text" name="name" id="contact_message_name" />
         </div>
         <div className="input email required contact_message_email">
           <label className="email required" htmlFor="contact_message_email">
@@ -50,7 +50,7 @@ export default class ContactForm extends React.Component {
             {' '}
             Email
           </label>
-          <input required className="string email required" type="email" name="contact_message[email]" id="contact_message_email" />
+          <input required className="string email required" type="email" name="email" id="contact_message_email" />
         </div>
         <div className="input text required contact_message_message">
           <label className="text required" htmlFor="contact_message_message">
@@ -58,10 +58,10 @@ export default class ContactForm extends React.Component {
             {' '}
             Message
           </label>
-          <textarea required rows="10" className="text required" name="contact_message[message]" id="contact_message_message" />
+          <textarea required rows="10" className="text required" name="message" id="contact_message_message" />
         </div>
         <Reaptcha sitekey="6Lf_HyoTAAAAADLFX3_kQLHLUMs-vtnLxleCxYgm" onVerify={this.verifyCallback} />
-        <input type="submit" name="commit" value="Send" className="btn" data-disable-with="Create Contact message" />
+        <input type="submit" name="commit" value="Send" className="btn" />
       </form>
     );
   }
