90e6204fb5600daed11b7e81e137e728893d5df8
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-22 22:01:26 +0100

    Submit contact form via ajax

--- frontend/src/components/contact_form.js
+++ frontend/src/components/contact_form.js
@@ -1,41 +1,55 @@
 import React from 'react';
 import Reaptcha from 'reaptcha';
 
-// import ContactMailer from '../services/contact_mailer';
-
 export default class ContactForm extends React.Component {
-  // state = {
-  //   recaptchaVerified: false,
-  // }
+  state = {
+    recaptchaVerified: false,
+    formSubmitted: false,
+  }
+
+  constructor() {
+    super();
+    this.formRef = React.createRef();
+  }
 
   verifyCallback = () => {
-    // this.setState({
-    //   recaptchaVerified: true,
-    // });
+    this.setState({
+      recaptchaVerified: true,
+    });
   }
 
-  handleSubmit = async () => {
-    // const { recaptchaVerified } = this.state;
-    // e.persist();
-    // e.preventDefault();
-    // const mailer = new ContactMailer();
-    // try {
-    //   await mailer.send(new FormData(e.target));
-    // } catch (ex) {
-    //   // eslint-disable-next-line no-console
-    //   console.error(ex);
-    // }
-    // e.preventDefault();
-    // if (!recaptchaVerified) {
-    //   // eslint-disable-next-line no-alert
-    //   window.alert('Please complete captcha verification first');
-    //   e.preventDefault();
-    // }
+  handleSubmit = async (e) => {
+    e.persist();
+    e.preventDefault();
+    const { recaptchaVerified } = this.state;
+
+    if (!recaptchaVerified) {
+      // eslint-disable-next-line no-alert
+      window.alert('Please complete captcha verification first');
+      return;
+    }
+
+    const url = this.formRef.current.action;
+    const data = new FormData(this.formRef.current);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
+
+    if (response.ok) {
+      this.setState({
+        formSubmitted: true,
+      });
+    } else {
+      // eslint-disable-next-line no-alert
+      window.alert('Message sending failed!');
+    }
   }
 
-  render() {
+  form() {
     return (
-      <form noValidate onSubmit={this.handleSubmit} className="simple_form form" id="new_contact_message" action="https://vv7mpokoyg.execute-api.eu-west-1.amazonaws.com/dev/contact" acceptCharset="UTF-8" method="post">
+      <form ref={this.formRef} noValidate onSubmit={this.handleSubmit} className="simple_form form" id="new_contact_message" action="https://vv7mpokoyg.execute-api.eu-west-1.amazonaws.com/dev/contact" acceptCharset="UTF-8" method="post">
         <div className="input string required contact_message_name">
           <label className="string required" htmlFor="contact_message_name">
             <abbr title="required">*</abbr>
@@ -63,6 +77,15 @@ export default class ContactForm extends React.Component {
         <Reaptcha sitekey="6Lf_HyoTAAAAADLFX3_kQLHLUMs-vtnLxleCxYgm" onVerify={this.verifyCallback} />
         <input type="submit" name="commit" value="Send" className="btn" />
       </form>
+    );
+  }
+
+  render() {
+    const { formSubmitted } = this.state;
+    return (
+      <React.Fragment>
+        { formSubmitted ? <p>Message sent successfully!</p> : this.form() }
+      </React.Fragment>
     );
   }
 }
