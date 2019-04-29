0a546eaae7561001933a6a6cfc2056232dba377b
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2019-04-29 16:17:51 +0100

    Allow different sizes of photos in gallery

--- frontend/src/pages/photos.js
+++ frontend/src/pages/photos.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import classNames from 'classnames';
 
 import Layout from '../components/layout';
 
-const Image = ({ thumbnailUrl, url }) => (
-  <a className="js-gallery-link gallery__link gallery__link--small" href={url}>
+/**
+ * @param {string} name name of image
+ */
+function imageSizeFromName(name) {
+  if (name.endsWith('_medium')) {
+    return 'medium';
+  }
+  if (name.endsWith('_large')) {
+    return 'large';
+  }
+  return 'small';
+}
+
+const Image = ({
+  thumbnailUrl, url, name, size,
+}) => (
+  <a data-name={name} className={classNames('js-gallery-link', 'gallery__link', `gallery__link--${size}`)} href={url}>
     <img className="gallery__image" style={{ minWidth: '100px', minHeight: '100px' }} alt="" src={thumbnailUrl} />
   </a>
 );
@@ -14,8 +30,10 @@ const Photos = ({ data }) => (
     <div className="gallery">
       { data.allFile.edges.map(edge => (
         <Image
+          key={edge.node.name}
           thumbnailUrl={edge.node.childImageSharp.resize.src}
           url={edge.node.childImageSharp.original.src}
+          size={imageSizeFromName(edge.node.name)}
         />
       )) }
     </div>
@@ -29,9 +47,10 @@ export const query = graphql`
     allFile(sort: {order: DESC, fields: [name]}, filter: {relativePath: {regex: "/photos/"}}) {
       edges {
         node {
+          name
           childImageSharp {
             id
-            resize(width: 200, height: 200, quality: 80) {
+            resize(width: 600, height: 600, quality: 80) {
               width
               height
               src
