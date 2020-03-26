caba211392359caf4fe4b9757c448d17a2bcd9dd
Leslie Hoare <iam@lesleh.co.uk>
2020-03-26 13:00:13 +0000

    Remove counter

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,43 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
 import Layout from '../components/layout';
-
-const useCounter = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = useCallback(() => setCount(count + 1), [count, setCount]);
-  const decrement = useCallback(() => setCount(count - 1), [count, setCount]);
-
-  return [count, increment, decrement];
-};
-
-const Counter = () => {
-  const [count, increment, decrement] = useCounter();
-
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    const id = requestAnimationFrame(() => {
-      increment();
-    });
-    // eslint-disable-next-line no-undef
-    return () => cancelAnimationFrame(id);
-  }, [count, increment]);
-
-  return (
-    <div>
-      <button type="button" onClick={decrement}>-</button>
-      {' '}
-      {count}
-      {' '}
-      <button type="button" onClick={increment}>+</button>
-    </div>
-  );
-};
 
 const About = () => (
   <Layout title="About">
     <p>About</p>
-    <Counter />
   </Layout>
 );
 
